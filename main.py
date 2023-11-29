2c09ed982aac695605eed11f4c2406eb2cbbb95a
Brian Mikolajczyk <brianm12@gmail.com>
2023-11-29 00:37:48 -0500

    Added argparser

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
+import argparse
 import csv
 
 
-def main():
+def user():
+    start = int(input("Enter a number\n"))
+    one(start)
+
+
+def one(start):
+    if start < 0 or start > 9999:
+        raise ValueError(f'{start} is not between 0 and 10000')
+
+    start = [i for i in str(start)]
+    while len(start) != 4:
+        start.insert(0, '0')
+
+    count = 0
+    diff = 0
+    tabs = 0
+    current = start  # Retains original start
+    print(f'Starting at:\n{int("".join(start))}')
+    while diff != 6174:
+        high = [i for i in reversed(sorted(current))]
+        low = sorted(current)
+        high_int = int("".join(high))
+        low_int = int("".join(low))
+
+        diff = high_int - low_int
+        current = [str(i) for i in str(diff)]
+
+        # Append leading 0s if necessary
+        while len(current) != 4:
+            current.insert(0, '0')
+
+        if len(set(start)) < 2:
+            break
+
+        count += 1
+        tabs = count
+        print('\t' * tabs, f'{high_int}-{low_int}={diff}')
+
+    if diff != 6174:
+        print('Unsolvable')
+    else:
+        print(f'Total Iterations: {count}')
+
+
+def allNumbers():
     results = {}
     for num in range(0, 10000):
         start = [i for i in str(num)]
@@ -15,9 +60,7 @@ def main():
 
         count = 0
         diff = 0
-        tabs = 0
         current = start  # Retains original start
-        # print(f'Starting at:\n{int("".join(start))}')
         while diff != 6174:
             high = [i for i in reversed(sorted(current))]
             low = sorted(current)
@@ -35,20 +78,35 @@ def main():
                 break
 
             count += 1
-            # tabs = count
-            # print('\t' * tabs, f'{high_int}-{low_int}={diff}')
 
         if diff != 6174:
             results[num] = 'Unsolvable'
         else:
             results[num] = count
-        # print(f'Total Iterations: {count}')
 
     with open('Results.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for k, v in results.items():
             writer.writerow([k, v])
 
+    print('Done!')
+
+
+def main(**kwargs):
+    if kwargs.get('all'):
+        allNumbers()
+    elif kwargs.get('num'):
+        one(kwargs.get('num'))
+    else:
+        user()
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('--all', '-a', action='store_true')
+    group.add_argument('--input', '-i', action='store_true')
+    group.add_argument('--num', '-n', type=int, nargs=1)
+
+    main(**vars(parser.parse_args()))
